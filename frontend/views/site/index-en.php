42245a6e4f68ae077252bbdcf47b884ecc2d40e3
NadineAstakhova <nadine.astakhova@gmail.com>
2017-06-27 23:47:05 +0300

    photo in profile

---
+++ frontend/views/site/index-en.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nadine
+ * Date: 27.06.2017
+ * Time: 20:42
+ */
+
+echo "EN";
---
+++ frontend/views/site/index-ru.php
@@ -0,0 +1,17 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Nadine Astakhova';
+?>
+<div class="row">
+<div class="info">
+    <img src="../../images/profile-foto1.png" alt="альтернативный текст" class="nk-img-stretch">
+    <p>Привет, Я Надя, начинающий программист</p>
+    <p>Сайт создан с целью дополнительной практики</p>
+    <p>Здесь вы можете увидеть моё портфолио</p>
+    <p>А также прочитать мои мысли о разных вещах</p>
+</div>
+
+
+</div>
