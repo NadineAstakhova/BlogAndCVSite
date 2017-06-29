223bcd43c7252dd6a87dec659e53f739b366eca7
NadineAstakhova <nadine.astakhova@gmail.com>
2017-06-29 17:55:52 +0300

    background 1

--- frontend/views/site/index-ru.php
+++ frontend/views/site/index-ru.php
@@ -5,13 +5,21 @@
 $this->title = 'Nadine Astakhova';
 ?>
 <div class="row">
-<div class="info">
-    <img src="../../images/profile-foto1.png" alt="альтернативный текст" class="nk-img-stretch">
-    <p>Привет, Я Надя, начинающий программист</p>
-    <p>Сайт создан с целью дополнительной практики</p>
-    <p>Здесь вы можете увидеть моё портфолио</p>
-    <p>А также прочитать мои мысли о разных вещах</p>
-</div>
+
+
+    <div class="info-all">
+        <div class="col-lg-4">
+            <img src="../../images/profile-foto1.png" alt="фото профайла" class="nk-img-stretch" >
+        </div>
+        <div class="col-lg-6">
+            <div class="info">
+                <p>Привет, Я Надя, начинающий программист</p>
+                <p>Сайт создан с целью дополнительной практики</p>
+                <p>Здесь вы можете увидеть моё портфолио</p>
+                <p>А также прочитать мои мысли о разных вещах</p>
+            </div>
+        </div>
+    </div>
 
 
 </div>
